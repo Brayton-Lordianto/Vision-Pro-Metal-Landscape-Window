8af24c102796896fa40d89d83e87950d656e201c
Brayton-Lordianto <braylord30@gmail.com>
2025-07-06 16:01:12 -0700

    Initial Commit

---
+++ PaintingLandscape/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  PaintingLandscape
+//
+//  Created by Brayton Lordianto on 7/6/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
